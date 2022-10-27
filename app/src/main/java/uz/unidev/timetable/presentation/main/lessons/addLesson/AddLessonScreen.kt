f496ce107443d0a01e5e1c53e91813399912444d
Nurlibay Koshkinbaev <koshkinbayevn@gmail.com>
2022-10-27 14:52:23 +0500

    add lesson screen being added

---
+++ app/src/main/java/uz/unidev/timetable/presentation/main/lessons/addLesson/AddLessonScreen.kt
@@ -0,0 +1,11 @@
+package uz.unidev.timetable.presentation.main.lessons.addLesson
+
+import androidx.fragment.app.Fragment
+import uz.unidev.timetable.R
+
+/**
+ *  Created by Nurlibay Koshkinbaev on 27/10/2022 14:46
+ */
+
+class AddLessonScreen: Fragment(R.layout.screen_add_lesson) {
+}
